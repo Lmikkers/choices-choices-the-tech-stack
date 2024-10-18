b38ff4687be0fb76e587b2e306a173e019bcebde
Larissa <larissa.mikkers@hotmail.com>
2024-10-18 15:26:31 +0200

    Contact pagina aangemaakt
    Ik heb gekeken in de docs hoe ik een pagina kan aanmaken,  als voorbeeld heb ik een contact pagina aangemaakt.

---
+++ my-next-app/src/app/contact/page.js
@@ -0,0 +1,11 @@
+// app/contact/page.js
+export default function Contact() {
+    return (
+      <section>
+        <article>
+          <h1>Neem contact met ons op</h1>
+          <p>We horen graag van je. Vul het formulier in of stuur ons een bericht.</p>
+        </article>
+      </section>
+    );
+  }
